e6051da70e1577432c680e5831d9238c50d8bfef
ElyasYid <elyassayle27@gmail.com>
2023-12-13 03:01:32 -0800

    laaa

---
+++ do_nada.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+  * do_nada - do nothing
+  * @head: stack head
+  * @line_n: line_number
+  * Return: no return
+ */
+
+void do_nada(stack_t **head, unsigned int line_n)
+{
+	(void) head;
+	(void) line_n;
+}
